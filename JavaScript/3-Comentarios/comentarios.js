c4167ada44ed3a8593b1e9bebe5df9eab4d5fc11
Luis Augusto <luisaucossousa@gmail.com>
2022-09-28 00:06:50 -0300

    Comentários e mesclando JS e HTML

---
+++ JavaScript/3-Comentarios/comentarios.js
@@ -0,0 +1,17 @@
+// os comentarios são anotações  que são ignorados
+console.log('Olá mundo'); // hello world!!!!
+console.log('linha 1')
+console.log('linha 2')
+//comentátio
+console.log('linha 3')
+console.log('linha 4')
+console.log('linha 5')
+//vai ser ignorado
+
+//comentario loongo
+
+/*cometnario de bloco 
+esse comentario de bloco
+pode se estender para as mais diversas linhas 
+ */
+
---
+++ JavaScript/4-Mesclando JS e HTML/index.js
@@ -0,0 +1,4 @@
+  //Um comentário
+  console.log('Olá mundo!');
+  console.log('Este trecho sera exibido no console do navegador.');
+  alert('Olá mundo!');
